a9353472aa1e6da8002d2844b7b01788df9b9992
kkty39 <yingyitong39@gmail.com>
2024-07-19 11:18:22 -0700

    simplify ResponseBuilder

--- src/main/java/org/sfa/volunteer/util/ResponseBuilder.java
+++ src/main/java/org/sfa/volunteer/util/ResponseBuilder.java
@@ -5,20 +5,18 @@ import org.sfa.volunteer.dto.common.SaayamStatusCode;
 
 public class ResponseBuilder {
 
-    private static <T> SaayamResponse<T> buildSuccessResponse(SaayamStatusCode statusCode, String message, T data) {
-        return SaayamResponse.success(statusCode, message, data);
-    }
-
-    public static <T> SaayamResponse<T> buildSuccessResponse(SaayamStatusCode statusCode, Object[] args, T data) {
+    private static <T> SaayamResponse<T> buildSuccessResponse(SaayamStatusCode statusCode, T data, Object[] args) {
         String message = MessageSourceUtil.getMessage(statusCode.getCode(), args);
         return SaayamResponse.success(statusCode, message, data);
     }
 
     public static <T> SaayamResponse<T> buildSuccessResponse(SaayamStatusCode statusCode, T data) {
-        String message = MessageSourceUtil.getMessage(statusCode.getCode(), null);
-        return SaayamResponse.success(statusCode, message, data);
+        return buildSuccessResponse(statusCode, data, null);
     }
 
+    public static <T> SaayamResponse<T> buildSuccessResponse(SaayamStatusCode statusCode, Object[] args, T data) {
+        return buildSuccessResponse(statusCode, data, args);
+    }
 
     public static <T> SaayamResponse<T> buildErrorResponse(int httpStatusCode, SaayamStatusCode statusCode, String message) {
         return SaayamResponse.error(httpStatusCode, statusCode, message);
